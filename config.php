1be4a1b0f23c3e30557ad9f607f107e4a9a12099
liu kelin <314566990@qq.com>
2016-03-31 21:17:12 +0800

    配置文件

--- config.php
+++ config.php
@@ -11,8 +11,8 @@ $config = array(
 	//队列使用的redis
 	'redis'=>array(
 			'host' => '127.0.0.1', 
-			'port'=>'6379', 
-			'db'=>'0'
+			'port'=>6379, 
+			'db'=>0
 		),
 	//redis 队列key
 	'queue_key'=>'celery_startup_zset',
