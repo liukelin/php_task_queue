16b1d0a6bccef1165d2cf98e817a1824b827440a
liu kelin <314566990@qq.com>
2016-05-31 17:48:44 +0800

    demo

--- demo/test_client.php
+++ demo/test_client.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 创建任务
+ * 创建任务  （可运行多个进程）
  * @author: liukelin 314566990@qq.com
  */
 
@@ -8,6 +8,6 @@ include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'../parsley/parsley.php');
 
 //创建任务
 $c = new parsley();
-for ($i=0; $i<=110000; $i++){
+for ($i=0; $i<=1000000; $i++){
 	$c->apply_async('helloWorld',array($i));
 }
--- demo/test_system.php
+++ demo/test_system.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 消费任务
+ * 消费任务 （可运行多个进程）
  * @author: liukelin 314566990@qq.com
  */
 include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'../parsley/parsley.php');
